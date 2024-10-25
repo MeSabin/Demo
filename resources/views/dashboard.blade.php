de5b2037b6a37b0bec88a0d9700809bcf1efb7b1
Sabin Kaphle <kaphlesabin789@gmail.com>
2024-10-25 11:57:16 +0545

    Dashboard setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Dashboard</title>
-   @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-   <div class="text-blue-500 bg-gray-300 p-6 text-lg text-center">
-      <p>User Dashboard</p>
-      <a href="{{route('Logout')}}">Logout</a>
-   </div>
-   <p class="text-blue-500">Your Email: {{Auth::user()->email}}</p>
-   <p class="text-blue-500">Your Name: {{ Auth::user()->name}}</p>
+    <div>
+        <aside class="bg-gray-200 w-64 min-h-screen">
+            <div class="flex items-center gap-8 px-4 pt-4 mb-10">
+                <div>
+                    <img src="{{ asset('images/ecommerce.png') }}" alt="Image not found" class="w-16">
+                </div>
+                <div>
+                    <p class="font-bold text-lg">E-commerce</p>
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="" class="text-gray-500 font-semibold text-base pl-8">Product Category</a>
+                    </li>
+                    <li class="mt-4">
+                        <a href="" class="text-gray-500 font-semibold text-base pl-8">Products</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+        <div class="">
+            <header class="fixed left-[256px] top-0 right-0 bg-gray-200 py-5 px-2 flex i justify-between items-center">
+                <div class="ml-10 flex gap-8">
+                    <div>
+                        <p class="">User Dashboard</p>
+                    </div>
+                    <div>
+                        <p class="text-green-700 text-lg font-semibold ">Verified</p>
+                        @if (session('loginSuccess'))
+                            <p class="text-green-700">{{ session('loginSuccess') }}</p>
+                        @endif
+                    </div>
+                </div>
+                <div class="mr-6 cursor-pointer" id="profile">
+                    <img src="{{ asset('images/profile.png') }}" alt="Image not found" class="w-10">
+                </div>
+                <div class="hidden absolute top-16 right-6  border bg-white border-gray-500 text-sm shadow-lg rounded-md p-4"
+                    id="profile_modal">
+                    <p class="text-blue-500">Your Email: {{ Auth::user()->email }}</p>
+                    <p class="text-blue-500">Your Name: {{ Auth::user()->name }}</p>
+                    <a href="{{ route('Logout') }}" class="text-blue-500">Logout</a>
+
+                </div>
+            </header>
+        </div>
+    </div>
+
+    <script>
+        window.addEventListener('load', function() {
+            let profile = document.querySelector('#profile');
+            let profileModal = document.querySelector('#profile_modal');
+
+            profile.onclick = function() {
+                console.log('Cliked');
+                if (profileModal.style.display == 'block') {
+                    profileModal.style.display = 'none';
+                } else {
+                    profileModal.style.display = 'block';
+                }
+            }
+        });
+    </script>
 </body>
+
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,34 +8,41 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
+
 <body class="flex justify-center items-center min-h-screen">
     <div class=" shadow-lg rounded p-6">
         <h2 class="font-bold text-lg text-center">Login</h2>
-    <form action="{{route('loginUser')}}" method="post">
-        @csrf
-        <div class="flex flex-col pb-6">
-            <label for="">Email:</label>
-            <input name="email" type="text" class="border border-gray-400 w-72 p-1 rounded">
-            <span class="text-red-500">
-                @error('email')
-                    {{$message}}
-                @enderror
-            </span>
-        </div>
-        <div class="flex flex-col pb-6 relative">
-            <label for="">Password:</label>
-            <input name="password" type="password" id="password" class="border border-gray-400 p-1 rounded">
-            <span class="text-red-500">
-                @error('password')
-                    {{$message}}
-                @enderror
-            </span>
-            <img src="{{asset('images/hide.png')}}" id="eye" alt="Image not found" class="absolute h-5 w-5 right-3 top-8 cursor-pointer">
-        </div>
-        <button type="submit" class="bg-green-600 text-white text-base w-full rounded p-1 cursor-pointer">Login</button>
-    </form>
+        <form action="{{ route('loginUser') }}" method="post">
+            @csrf
+            <div class="flex flex-col pb-6">
+                <label for="">Email:</label>
+                <input name="email" type="text" class="border border-gray-400 w-72 p-1 rounded">
+                <span class="text-red-500">
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </span>
+            </div>
+            <div class="flex flex-col pb-6 relative">
+                <label for="">Password:</label>
+                <input name="password" type="password" id="password" class="border border-gray-400 p-1 rounded">
+                <span class="text-red-500">
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </span>
+                <img src="{{ asset('images/hide.png') }}" id="eye" alt="Image not found"
+                    class="absolute h-5 w-5 right-3 top-8 cursor-pointer">
+            </div>
+            <button type="submit"
+                class="bg-green-600 text-white text-base w-full rounded p-1 cursor-pointer">Login</button>
+            <div class="flex justify-center mt-2">
+                <a href="{{ route('register') }}" class="text-blue-500 text-sm text-center">Register</a>
+            </div>
+        </form>
     </div>
 
-   <script src="{{asset('js/passwordToggle.js')}}"></script>
+    <script src="{{ asset('js/passwordToggle.js') }}"></script>
 </body>
+
 </html>
